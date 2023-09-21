d36b55e5e31c84c967ca7a831def8d5c515cd3d9
Guo, Zheng(Alex) <zguo2@statestreet.com>
2023-09-21 14:03:55 +0000

    init-view

---
+++ install.sh
@@ -0,0 +1,6 @@
+pip3 install django djangorestframework django-cors-headers
+pip3 install azure-storage-blob azure-eventhub avro-python3
+pip3 install mysql-connector-python3pip3 install azure-identity azure-keyvault-secrets
+pip3 install langchain langchain_experimental
+pip3 install databricks-api
+pip3 install pymysql
